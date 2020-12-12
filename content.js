4a79268199c0743176c776eb74191dcf29046fa4
Raymel Francisco <franciscoraymel@gmail.com>
2020-12-13 06:24:18 +0800

    HEAD: Initial working code.

---
+++ content.js
@@ -0,0 +1,11 @@
+
+//document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    var videoObjYep = document.getElementsByTagName('twitch-live-player')
+    videoObjYep[0].remove()
+
+    var chatObjYep = document.getElementsByTagName('twitch-live-chat')
+    chatObjYep[0].remove()
+  }, 1000)
+//})	
+
