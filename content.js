5dff05182b7380857c87c6577aeb62e519656c7f
Raymel Francisco <franciscoraymel@gmail.com>
2020-12-17 04:58:47 +0800

    master: Temporary fix when elements are skipped due to manual timing. TODO: Proper sync timing where the extension should wait for DOM to be loaded, proper checks and a fallback repeat if things fail

--- content.js
+++ content.js
@@ -1,11 +1,34 @@
 
 //document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(function () {
+  const timeout_1 = setTimeout(function () {
     var videoObjYep = document.getElementsByTagName('twitch-live-player')
-    videoObjYep[0].remove()
+    if (videoObjYep[0]) {
+      videoObjYep[0].remove()
+    }
 
     var chatObjYep = document.getElementsByTagName('twitch-live-chat')
-    chatObjYep[0].remove()
+    if (chatObjYep[0]) {
+      chatObjYep[0].remove()
+    }
   }, 1000)
+  //clearTimeout(timeout_1)
+
+  const timeout_2 = setTimeout(function () {
+    // var videoObjYep = document.getElementsByTagName('twitch-live-player')
+    // videoObjYep[0].remove()
+
+    // var chatObjYep = document.getElementsByTagName('twitch-live-chat')
+    // chatObjYep[0].remove()
+    var videoObjYep = document.getElementsByTagName('twitch-live-player')
+    if (videoObjYep[0]) {
+      videoObjYep[0].remove()
+    }
+
+    var chatObjYep = document.getElementsByTagName('twitch-live-chat')
+    if (chatObjYep[0]) {
+      chatObjYep[0].remove()
+    }
+  }, 2000)
+  //clearTimeout(timeout_2)
 //})	
 
